ea79151d729a144f328b9c205dd2f365dbcbe640
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-06-12 23:54:34 +0300

    feature/Vanilla: completed a new lesson

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-};
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-};
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+navigator.geolocation.watchPosition((data) => {
+  console.log(data);
+  speed.textContent = data.coords.speed;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+  console.error(err);
+});
